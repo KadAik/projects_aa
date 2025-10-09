dcab2424d83be7d696aee46ea341b981851101c7
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-09 08:56:24 +0100

    Update urls.py
    Update urls.py in the root to display a simple message as the base domain is accessed.

--- p041725/urls.py
+++ p041725/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Benin Air Force projects!")),
     path('admin/', admin.site.urls),
     path('psycho/', include('psycho.urls')),
 ] + debug_toolbar_urls()
