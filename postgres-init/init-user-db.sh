2c5a9bc4a93c9880660edecccd08cd5012fa758e
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-14 12:39:55 +0100

    Add logging to database initialization script
    Added echo statements to provide feedback during database initialization and user creation. Also updated the shebang to use /bin/bash for consistency.

--- postgres-init/init-user-db.sh
+++ postgres-init/init-user-db.sh
@@ -1,8 +1,13 @@
-#! /usr/bin/env bash
+#! /bin/bash
 
 set -e
+
+echo "Initializing database..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE ROLE psycho_app WITH LOGIN PASSWORD '${DB_PASSWORD}';
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO psycho_app;
 EOSQL
+
+echo "Database initialized."
+echo "User psycho_app created with access to database $POSTGRES_DB."
