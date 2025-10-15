00f4bc4908ef10446cc1bcdf5c0e7be636de99b0
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-15 10:46:36 +0100

    Add default values for DB user and name in init script
    Updated init-user-db.sh to use default values for DB_USER and DB_NAME if environment variables are not set. Also changed the shebang to use /usr/bin/env bash for better portability.

--- postgres-init/init-user-db.sh
+++ postgres-init/init-user-db.sh
@@ -1,28 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 echo "Initializing database..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- Create user if not exists
+    -- Create the db user if not exists
     DO \$\$
     BEGIN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${DB_USER}') THEN
-            CREATE ROLE ${DB_USER} WITH LOGIN PASSWORD '${DB_PASSWORD}';
-            RAISE NOTICE 'Role ${DB_USER} created';
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${DB_USER:-psycho_app}') THEN
+            CREATE ROLE ${DB_USER:-psycho_app} WITH LOGIN PASSWORD '${DB_PASSWORD}';
+            RAISE NOTICE 'Role ${DB_USER:-psycho_app} created';
         ELSE
-            RAISE NOTICE 'Role ${DB_USER} already exists';
+            RAISE NOTICE 'Role ${DB_USER:-psycho_app} already exists';
         END IF;
     END \$\$;
 
     -- Create database if not exists
-    SELECT 'CREATE DATABASE ${DB_NAME} OWNER ${DB_USER}'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}')\gexec
+    SELECT 'CREATE DATABASE ${DB_NAME:-psycho_db} OWNER ${DB_USER:-psycho_app}'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME:-psycho_db}')\gexec
 
     -- Grant privileges
-    GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};
-    
+    GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME:-psycho_db} TO ${DB_USER:-psycho_app};
+
 EOSQL
 
 echo "Database initialized successfully."
-echo "User '${DB_USER}' has access to database '${DB_NAME}'."
+echo "User '${DB_USER:-psycho_app}' has access to database '${DB_NAME:-psycho_db}'."
