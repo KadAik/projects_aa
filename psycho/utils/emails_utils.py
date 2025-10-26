7fadf46718807f319b950b697ef4a78317d67f0c
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-26 21:10:33 +0100

    Update mail sending helper.
    Use premailer to transform html email so css are correctly set on emails accross all clients.

--- psycho/utils/emails_utils.py
+++ psycho/utils/emails_utils.py
@@ -1,8 +1,10 @@
+import email
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 import logging
+from premailer import transform
 
 from psycho.models import Application
 
@@ -20,6 +22,8 @@ def send_application_confirmation_email(application_id):
     }
 
     html_content = render_to_string("psycho/emails/application_submitted.html", context)
+    # Inline all CSS so styles are preserved across all mail clients
+    html_content = transform(html_content, remove_classes=True, strip_important=False)
     text_content = strip_tags(html_content)
 
     email = EmailMultiAlternatives(
@@ -30,6 +34,9 @@ def send_application_confirmation_email(application_id):
         reply_to=["support@psycho-tests.emaa.mil.bj"],
     )
     email.attach_alternative(html_content, "text/html")
+    email.content_subtype = "html"
+    email.mixed_subtype = "related"  # ensures proper multipart formatting
+    email.encoding = "utf-8"
 
     try:
         email.send()
