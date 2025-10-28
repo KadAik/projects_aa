4354f4ef1d16861d289fb2cfecfc67875a93f1bc
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-28 17:35:50 +0100

    Update compose and entrypoint
    Update compose : add back- and back-2 to allow host so that request can reach them.
    Update docker-entrypoint.sh to create a super user with sh instead createsuperuser which doesn't support --paswword.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,7 +29,20 @@ python manage.py migrate --noinput
 
 # Create default superuser if it doesn't exist
 echo "Creating default superuser if it doesn't exist..."
-python manage.py createsuperuser --username "${DJANGO_SUPERUSER_USERNAME}" --email "${DJANGO_SUPERUSER_EMAIL}" --password "${DJANGO_SUPERUSER_PASSWORD}" --noinput || echo "Superuser already exists, skipping..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+    User.objects.create_superuser(
+        username="${DJANGO_SUPERUSER_USERNAME}",
+        email="${DJANGO_SUPERUSER_EMAIL}",
+        password="${DJANGO_SUPERUSER_PASSWORD}",
+    )
+    print("Superuser created.")
+else:
+    print("Superuser already exists, skipping...")
+EOF
+
 
 # Get qcluster up and running for background tasks
 echo "Starting Django Q cluster..."
