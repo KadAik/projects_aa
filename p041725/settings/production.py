1e634b441797ecccfb5122b9c3c613ac7d6c5694
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-29 12:27:21 +0100

    Update production.py
    Add security settings to production settings.

--- p041725/settings/production.py
+++ p041725/settings/production.py
@@ -7,12 +7,6 @@ DEBUG = False
 
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default=["127.0.0.1", "localhost"])
-
-CORS_ALLOWED_ORIGINS = config(
-    "CORS_ALLOWED_ORIGINS", cast=Csv(), default=["127.0.0.1", "localhost"]
-)
-
 # Production database
 DATABASES = {
     "default": {
@@ -34,4 +28,31 @@ STATIC_ROOT = config(
 
 # Media files settings
 MEDIA_URL = config("MEDIA_URL", default="/media/")
-MEDIA_ROOT = config("MEDIA_ROOT", default="/media/")  # note not using BASE_DIR here
+MEDIA_ROOT = config(
+    "MEDIA_ROOT", default="/mediafiles/"
+)  # note not using BASE_DIR here
+
+# Security settings
+# Security Headers
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default=["127.0.0.1", "localhost"])
+
+CORS_ALLOWED_ORIGINS = config(
+    "CORS_ALLOWED_ORIGINS", cast=Csv(), default=["127.0.0.1", "localhost"]
+)
+
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default=[], cast=Csv())
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"  # Clickjacking protection
+
+
+# HTTPS Enforcement
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+# HTTP Strict Transport Security (HSTS):
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
