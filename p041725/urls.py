4008014858787a7769267cc99c90986c84d807ff
Kadolphe <159726232+KadAik@users.noreply.github.com>
2025-10-31 11:40:00 +0100

    Update urls.py
    Update urls to serve media files in dev.

--- p041725/urls.py
+++ p041725/urls.py
@@ -19,9 +19,15 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", lambda request: HttpResponse("Benin Air Force projects!")),
     path("admin/", admin.site.urls),
     path("psycho/", include("psycho.urls")),
-] + debug_toolbar_urls()
+]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
